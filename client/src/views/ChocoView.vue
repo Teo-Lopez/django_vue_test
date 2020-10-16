a89d5014ca44d12bc4b795549d3570a242543611
Teo <teodorolg130@gmail.com>
2020-10-16 21:57:39 +0200

    created FilledView draft and moved views to their own folder

---
+++ client/src/views/ChocoView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="cardWrapper" :style="{ backgroundImage: 'url(' + bgImage + ')' }">
+    <div class="cardInner">
+     <ChocoForm/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChocoForm from '../components/ChocoForm'
+
+export default {
+  name: 'ChocoView',
+  data () {
+    return {
+      bgImage: screen.width > 2000 ? require('../assets/womenInField_2400x1800.jpg') : require('../assets/womenInField_1920x1440.jpg'),
+      chocoCover: {
+        front: require('../assets/chocoCoverFront.jpg'),
+        back: require('../assets/chocoCoverBack.jpg')
+      }
+    }
+  },
+  components: {ChocoForm}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.cardWrapper {
+  display: flex;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 90vh;
+  justify-content: center;
+  align-items: center;
+}
+.cardInner {
+  background-color: rgba(240,240,240,0.10);
+  padding: 20px;
+  color: white;
+  width: 50%;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
